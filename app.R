dadb95f433dc47dcd954747288af40d7dde568f3
Paul McElhany <paul.mcelhany@noaa.gov>
2026-06-12 15:37:59 -0700

    fixed issue with rendering reports in published version

--- app.R
+++ app.R
@@ -4,12 +4,27 @@
 
 # QUARTO_PATH !!!! -----------------------
 # QUARTO_PATH must be set for reports
-# This is not an issue on the local machine, but is not globally set on the server
-# This is the path on the NFMS Posit server for hosting shiny apps
-Sys.setenv(QUARTO_PATH="/opt/quarto/latest/bin/quarto")
-# This is the path on Paul's mac and can be used to reset the env variable
-# if it get accidentally set for the server
-# Sys.setenv(QUARTO_PATH="/usr/local/bin/quarto")
+
+# This is the path used the NFMS Posit server
+# The code points to a copy of quarto version 1.5.57 installed within the shiny app.
+# The version 1.5.57 is needed because the version 1.9.37 installed on the server cause the app to crash when rendering quarto
+# 1. Define paths
+quarto_base_dir <- file.path(getwd(), "quarto-1.5.57", "bin")
+local_quarto <- file.path(quarto_base_dir, "quarto")
+# 2. BULK FIX ALL PERMISSIONS:
+# This recursively finds EVERY file inside the quarto bin folder
+# (including deno, sass, pandoc, etc.) and makes them executable on the linex server.
+if (dir.exists(quarto_base_dir)) {
+  all_binaries <- list.files(quarto_base_dir, recursive = TRUE, full.names = TRUE)
+  Sys.chmod(all_binaries, mode = "0755")
+}
+# 3. Point R to the local version
+#Sys.setenv(QUARTO_PATH = local_quarto)
+
+
+# This is the path on Paul's mac and can be used to set the env variable
+# for rendering quarto on the local machine.
+Sys.setenv(QUARTO_PATH="/usr/local/bin/quarto")
 
 pkgload::load_all(export_all = TRUE,helpers = FALSE,attach_testthat = FALSE)
 options( "golem.app.prod" = TRUE)
